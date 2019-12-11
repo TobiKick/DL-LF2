66c602462abe42e7886b6d391fa3aaf69c3487bd
Ubuntu <tk93@NvidiaTF.rmkvgvhfovxupp35czr4lchxxb.jx.internal.cloudapp.net>
2019-12-11 16:35:26 +0000

    update on reward structure

--- app.py
+++ app.py
@@ -185,8 +185,8 @@ if __name__ == "__main__":
 
             if(env.get_detail() != None):
                 player_info = getPlayerInformation(env.get_detail())
-                if done == True & env.get_detail()[0].get('hp') == 0:
-                    reward = -10
+		if done == False and env.get_detail()[0].get('hp') == 0:
+			reward = -10
             print("Reward: " + str(reward))
             _next_state = np.reshape(next_state, (1, state_size_x, state_size_y, 4))
             _player_info = np.reshape(player_info, (1, 16))
